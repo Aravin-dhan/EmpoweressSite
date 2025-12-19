00f0c552d67f801961cf5882e411195423f965ec
Aravindhan <codfish_dulcet.3s@icloud.com>
2025-12-19 16:29:14 +0530

    chore: remove temporary debug script

--- scripts/debug-post.ts
+++
@@ -1,8 +0,0 @@
-import { getPostBySlug } from "@/lib/mdx";
-
-async function main() {
-  const post = await getPostBySlug("book-review-queer-judgments");
-  console.log(JSON.stringify(post, null, 2));
-}
-
-main();
